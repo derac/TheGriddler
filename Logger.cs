d7789748e662ca5a708ed7f73a39079f55ba7768
derac <dwolson123@gmail.com>
2026-01-28 14:54:14 -0600

    prep for release

--- Logger.cs
+++ Logger.cs
@@ -5,24 +5,30 @@ namespace TheGriddler;
 
 public static class Logger
 {
+#if DEBUG
     private static string _logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "debug.log");
+#endif
 
     static Logger()
     {
+#if DEBUG
         try
         {
             if (File.Exists(_logPath)) File.Delete(_logPath);
         }
         catch { }
+#endif
     }
 
     public static void Log(string message)
     {
+#if DEBUG
         try
         {
             string timestamp = DateTime.Now.ToString("HH:mm:ss.fff");
             File.AppendAllText(_logPath, $"[{timestamp}] {message}{Environment.NewLine}");
         }
         catch { }
+#endif
     }
 }
